f48b5d51a3b1645aab831545aa470f79893ef46f
Pulatoff <niyozbekpulatov@gmail.com>
2022-08-29 22:44:30 +0500

    shemas writed

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -19,6 +19,88 @@ const typeDefs = gql`
     Consumer
   }
 
+  enum WeightMeasure {
+    kg
+    lb
+  }
+  type Weight {
+    measure: WeightMeasure!
+    weight: Int!
+  }
+
+  enum HeightMeasure {
+    cm
+    inch
+  }
+
+  type Height {
+    height: Int!
+    measure: HeightMeasure!
+  }
+
+  type Consumer {
+    id: ID!
+    weight: Weight!
+    height: Height!
+    favorite_foods: [String!]!
+    least_favorite_foods: [String!]!
+    user: User!
+    dairy: [Dairy]!
+    goals: [Goals]!
+    status: Status!
+  }
+
+  type Status {
+    id: ID!
+    expires_date: String!
+    version: Version!
+  }
+
+  type Version {
+    id: ID!
+    category: Category!
+  }
+
+  enum Category {
+    Free
+    Premium
+  }
+
+  type Goals {
+    id: ID!
+    carbohydrates: String!
+    protein: String!
+    fats: String!
+    colories: String!
+    start_date: String!
+    end_date: String!
+  }
+
+  type Dairy {
+    quantity_id: Int!
+    serving_id: Int!
+    course: Coure!
+    programm: Programm!
+  }
+
+  type Programm {
+    quantity_id: Int!
+    serving_id: Int!
+    course: Coure!
+  }
+
+  enum Coure {
+    Breakfast
+  }
+
+  type Personal_Trainer {
+    user: User!
+    programms: [Programm]!
+    status: Boolean!
+    consumer: [Consumer!]!
+    link: String!
+  }
+
   type Query {
     users: [User!]!
   }
