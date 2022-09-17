74afcd17854543f4debaeb17256515e94d1dc728
Pulatoff <niyozbekpulatov@gmail.com>
2022-09-17 11:14:46 +0500

    server connected to db

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config({});
 const { ApolloServer } = require("apollo-server");
 const { resolvers } = require("./schema/resolvers");
 const { typeDefs } = require("./schema/typeDefs");
-
+const sequlize = require("./configs/db");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+sequlize.sync();
 
 server.listen().then(({ url }) => {
   console.log(`Server running in: ${url}`);
