ca8b551544be301c457c057827122af41bdc28b8
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-06 11:37:52 +0500

    signin writed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel')
 const createJwt = require('../utils/createJWT')
+const bcrypt = require('bcryptjs')
+
 exports.signup = async (req, res, next) => {
     try {
         const { first_name, last_name, email, password, passwordConfirm } = req.body
@@ -7,16 +9,35 @@ exports.signup = async (req, res, next) => {
         if (password !== passwordConfirm) throw new Error('password not the same')
         // checking user existing
         const user = await User.create({ first_name, last_name, email, password })
-        const jwt = await createJwt(user.id)
+        const token = await createJwt(user.id)
         res.status(200).json({
             status: 'success',
             data: {
-                accessToken: `Bearer ${jwt}`,
+                accessToken: `Bearer ${token}`,
                 user,
             },
         })
-    } catch (e) {
-        console.log(e)
-        res.status(404).json({ message: e.message })
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({ message: error.message })
+    }
+}
+exports.signin = async (req, res, next) => {
+    try {
+        const { password, email } = req.body
+        if (!password || !email) throw new Error('field could not be pustim')
+        const user = await User.findOne({ where: { email } })
+        if (!user) throw new Error('Wrong password or email, Please try again')
+        // comparing passwords
+        const compare = await bcrypt.compare(password, user.password)
+        if (!compare) throw new Error('Wrong password or email, Please try again')
+        const token = createJwt(user.id)
+        res.status(200).json({ status: 'succes', data: { user, accessToken: `Bearer ${token}` } })
+    } catch (error) {
+        console.log(error.message)
+        res.status(404).json({
+            status: 'failed',
+            message: error.message,
+        })
     }
 }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,5 +2,6 @@ const authController = require('../controllers/authController')
 const route = require('express').Router()
 
 route.route('/signup').post(authController.signup)
+route.route('/signin').post(authController.signin)
 
 module.exports = route
