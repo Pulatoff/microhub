e83ae980e6f94fa179281ee78587241d7d722202
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-07 17:12:52 +0500

    products added/

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,6 @@ module.exports = (error, req, res, next) => {
     res.status(error.statusCode).json({
         message: error.message,
         status: error.status,
-        slack: error.slack,
+        stack: error.stack,
     })
 }
--- controllers/programController.js
+++ controllers/programController.js
@@ -3,7 +3,6 @@ const AppError = require('../utils/AppError')
 
 exports.addProgram = async (req, res, next) => {
     try {
-        console.log(111)
         const { course, serving, food_id, quantity } = req.body
         const program = await Program.create({ course, serving, food_id, quantity, wpPersonalTrainerId: 1 })
         res.status(200).json({ status: 'success', data: { program } })
