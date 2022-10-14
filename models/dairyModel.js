bb7735e46f8b62e838cd56bb2d548515efd13a67
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-14 08:37:09 +0500

    some updates in dairies controllers

--- models/dairyModel.js
+++ models/dairyModel.js
@@ -19,4 +19,7 @@ const Dairy = sequelize.define('dairies', {
 Program.hasOne(Dairy, { as: 'program' })
 Dairy.belongsTo(Program, { as: 'program' })
 
+Consumer.hasOne(Dairy, { as: 'consumer' })
+Dairy.belongsTo(Consumer, { as: 'consumer' })
+
 module.exports = Dairy
