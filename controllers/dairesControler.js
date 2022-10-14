6c1ad94094654fab6cd81c9057b7cd16d201b4f7
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-14 08:32:40 +0500

    some updates in controllers

--- controllers/dairesControler.js
+++ controllers/dairesControler.js
@@ -14,22 +14,22 @@ exports.addDairy = async (req, res, next) => {
 
 exports.getDairies = async (req, res, next) => {
     const { course, serving, food_id, quantity, programId } = req.body
-    const program = await Dairy.create({
+    const dairies = await Dairy.create({
         course,
         serving,
         food_id,
         quantity,
         programId,
     })
-    res.status(200).json({ status: 'success', data: { program } })
+    res.status(200).json({ status: 'success', data: { dairies } })
 }
 
 exports.getDairy = async (req, res, next) => {
-    const program = await Dairy.findAll()
-    res.status(200).json({ status: 'success', data: { program } })
+    const dairies = await Dairy.findAll()
+    res.status(200).json({ status: 'success', data: { dairies } })
 }
 
 exports.getOneDairy = async (req, res, next) => {
-    const program = await Dairy.findByPk(req.params.id)
-    res.status(200).json({ status: 'success', data: { program } })
+    const dairies = await Dairy.findByPk(req.params.id)
+    res.status(200).json({ status: 'success', data: { dairies } })
 }
