d40306bfd2f021e9085a088757c8b9e8a387dbe3
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-15 10:55:25 +0500

    dairy update

--- controllers/dairesControler.js
+++ controllers/dairesControler.js
@@ -1,40 +1,66 @@
 const Dairy = require('../models/dairyModel')
+const AppError = require('../utils/AppError')
 
 exports.addDairy = async (req, res, next) => {
-    const { course, serving, food_id, quantity, programId } = req.body
-    const program = await Dairy.create({
-        course,
-        serving,
-        food_id,
-        quantity,
-        programId,
-    })
-    res.status(200).json({ status: 'success', data: { program } })
+    try {
+        const { course, serving, food_id, quantity, programId } = req.body
+        const program = await Dairy.create({
+            course,
+            serving,
+            food_id,
+            quantity,
+            programId,
+        })
+        res.status(200).json({ status: 'success', data: { program } })
+    } catch (error) {
+        console.log(error)
+        next(new AppError(error.message, 404))
+    }
 }
 
 exports.getDairies = async (req, res, next) => {
-    const { course, serving, food_id, quantity, programId } = req.body
-    const dairies = await Dairy.create({
-        course,
-        serving,
-        food_id,
-        quantity,
-        programId,
-    })
-    res.status(200).json({ status: 'success', data: { dairies } })
+    try {
+        const { course, serving, food_id, quantity, programId } = req.body
+        const dairies = await Dairy.create({
+            course,
+            serving,
+            food_id,
+            quantity,
+            programId,
+        })
+        res.status(200).json({ status: 'success', data: { dairies } })
+    } catch (error) {
+        console.log(error)
+        next(new AppError(error.message, 404))
+    }
 }
 
 exports.getDairy = async (req, res, next) => {
-    const dairies = await Dairy.findAll()
-    res.status(200).json({ status: 'success', data: { dairies } })
+    try {
+        const dairies = await Dairy.findAll()
+        res.status(200).json({ status: 'success', data: { dairies } })
+    } catch (error) {
+        console.log(error)
+        next(new AppError(error.message, 404))
+    }
 }
 
 exports.getOneDairy = async (req, res, next) => {
-    const dairies = await Dairy.findByPk(req.params.id)
-    res.status(200).json({ status: 'success', data: { dairies } })
+    try {
+        const dairies = await Dairy.findByPk(req.params.id)
+        res.status(200).json({ status: 'success', data: { dairies } })
+    } catch (error) {
+        console.log(error.message)
+        next(new AppError(error.message, 404))
+    }
 }
 
-exports.updateDairy = async(req,res,next)=>{
-    const dairy = await Dairy.update(req.body,{where:{id:req.params.id}})
-    res.status(200).json({ status: 'success', data: { dairy } })
+exports.updateDairy = async (req, res, next) => {
+    try {
+        const dairy = await Dairy.update(req.body, { where: { id: req.params.id } })
+        res.status(200).json({ status: 'success', data: { dairy } })
+    } catch (error) {
+        console.log(error)
+        next(new AppError(error.message, 404))
+    }
 }
