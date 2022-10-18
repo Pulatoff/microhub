fc3e734eae7d96d357c356958b7cc5b180b15f95
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-18 14:33:56 +0500

    some updates in micrihub

--- middlewares/app.js
+++ middlewares/app.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const app = express()
+const AppError = require('../utils/AppError')
 const userRouter = require('../routes/authRouter')
 const consumerRouter = require('../routes/consumerRoutes')
 const programRouter = require('../routes/programRouter')
 const errorController = require('../controllers/errorController')
-const AppError = require('../utils/AppError')
 const TrainerRouter = require('../routes/trainerRouter')
 const DairyController = require('../routes/dairyRouter')
 const GoalsRouter = require('../routes/goalRoutes')
 
 app.use(express.json())
 
+// main routes
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/consumers', consumerRouter)
 app.use('/api/v1/programs', programRouter)
 app.use('/api/v1/trainers', TrainerRouter)
-app.use('/api/v1/daires', DairyController)
+app.use('/api/v1/diaries', DairyController)
 app.use('/api/v1/goals', GoalsRouter)
+
 app.all('*', (req, res, next) => {
     next(new AppError('Page not Found', 404))
 })
--- models/dairyModel.js
+++ models/dairyModel.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize')
 const sequelize = require('../configs/db')
 const Program = require('../models/programModel')
 const Consumer = require('./consumerModel')
-const Dairy = sequelize.define('dairies', {
+const Dairy = sequelize.define('diaries', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     course: {
         type: DataTypes.ENUM('breakfast', 'brunch', 'lunch', 'snacks', 'dinner'),
