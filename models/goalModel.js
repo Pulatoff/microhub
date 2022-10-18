c6e78bb1b78bf7c8bee8e6723b1342e4cf9b2f4d
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-18 17:09:47 +0500

    goals model updated

--- models/goalModel.js
+++ models/goalModel.js
@@ -2,15 +2,23 @@ const sequelize = require('../configs/db')
 const { DataTypes } = require('sequelize')
 const Consumer = require('../models/consumerModel')
 
-const Goals = sequelize.define('goals', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    protein: { type: DataTypes.FLOAT },
-    carbohydrates: { type: DataTypes.FLOAT },
-    fats: { type: DataTypes.FLOAT },
-    calories: { type: DataTypes.FLOAT },
-    start_date: { type: DataTypes.STRING, allowNull: false },
-    end_date: { type: DataTypes.STRING, allowNull: false },
-})
+const Goals = sequelize.define(
+    'goals',
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        protein: { type: DataTypes.FLOAT },
+        carbohydrates: { type: DataTypes.FLOAT },
+        fats: { type: DataTypes.FLOAT },
+        calories: { type: DataTypes.FLOAT },
+        start_date: { type: DataTypes.STRING, allowNull: false },
+        end_date: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+        timestamps: true,
+        createdAt: true,
+        updatedAt: false,
+    }
+)
 
 Consumer.hasMany(Goals)
 Goals.belongsTo(Consumer)
