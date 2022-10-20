5e8e5d225a7991a2a1bfee90a9b5fc5378e73a51
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-20 17:22:14 +0500

    one get route group writed

--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/groupController')
 const auth = require('../controllers/authController')
 
 router.route('/').post(auth.protect, controller.addGroup).get(auth.protect, controller.getAllGroups)
+router.route('/:id').get(auth.protect, controller.getOneGroup)
 
 router.route('/consumer').post(auth.protect, controller.bindGroup)
 
