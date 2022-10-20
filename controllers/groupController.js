4aef800aa73c32c6802568f4a37d208e07e50c64
Pulatoff <niyozbekpulatov@gmail.com>
2022-10-20 16:31:12 +0500

    binding groups success

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -2,6 +2,7 @@ const Group = require('../models/groupModel')
 const GroupConsumer = require('../models/groupConsumerModel')
 const AppError = require('../utils/AppError')
 const Trainer = require('../models/personalTrainerModel')
+const ConsumerTrainer = require('../models/consumerTrainer')
 
 exports.addGroup = async (req, res, next) => {
     try {
@@ -12,6 +13,26 @@ exports.addGroup = async (req, res, next) => {
         res.status(200).json({
             status: 'success',
             data: { group: { id: group.id, name: group.name } },
+        })
+    } catch (error) {
+        next(new AppError(error.message, 404))
+    }
+}
+
+exports.bindGroup = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+        const trainer = await Trainer.findOne({ where: { userId } })
+
+        const { consumerId, groupId } = req.body
+        const group = await Group.findByPk(groupId, { where: { nutritionistId: trainer.id } })
+        if (!group) next(new AppError('this group not found', 404))
+        const consumer = await ConsumerTrainer.findOne({ where: { nutritionistId: trainer.id, consumerId } })
+        if (!consumer) next(new AppError('This consumer binding to you', 404))
+        await GroupConsumer.create({ groupId, consumerId })
+        res.status(200).json({
+            status: 'success',
+            data: '',
         })
     } catch (error) {
         next(new AppError(error.message, 404))
--- routes/groupRouter.js
+++ routes/groupRouter.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const controller = require('../controllers/groupController')
+const auth = require('../controllers/authController')
 
-router.route('/').post(controller.addGroup)
+router.route('/').post(auth.protect, controller.addGroup)
+
+router.route('/consumer').post(auth.protect, controller.bindGroup)
 
 module.exports = router
