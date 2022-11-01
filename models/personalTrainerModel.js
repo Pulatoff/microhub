171810bd4c25b63e2c96475e7acb69fe1cb20c23
Pulatoff <niyozbekpulatov@gmail.com>
2022-11-01 17:56:45 +0500

    some updates in model nutritionist

--- models/personalTrainerModel.js
+++ models/personalTrainerModel.js
@@ -3,11 +3,19 @@ const { DataTypes } = require('sequelize')
 const User = require('../models/userModel')
 const crypto = require('crypto')
 
-const Personal_treiner = sequelize.define('nutritionist', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    linkToken: { type: DataTypes.STRING },
-    credentials: { type: DataTypes.INTEGER },
-})
+const Personal_treiner = sequelize.define(
+    'nutritionist',
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        linkToken: { type: DataTypes.STRING },
+        credentials: { type: DataTypes.INTEGER },
+    },
+    {
+        timestamps: true,
+        createdAt: true,
+        updatedAt: false,
+    }
+)
 
 User.hasOne(Personal_treiner, { onDelete: 'CASCADE' })
 Personal_treiner.belongsTo(User, { onDelete: 'CASCADE' })
