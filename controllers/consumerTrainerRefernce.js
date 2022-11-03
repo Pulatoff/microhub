3527ac8200e1350a59cddc4dd0dabf334c70f30e
Pulatoff <niyozbekpulatov@gmail.com>
2022-11-03 20:37:33 +0500

    some updates

--- controllers/consumerTrainerRefernce.js
+++ controllers/consumerTrainerRefernce.js
@@ -3,21 +3,14 @@ const ConsumerTrainer = require('../models/consumerTrainer')
 const Trainer = require('../models/personalTrainerModel')
 // utils
 const AppError = require('../utils/AppError')
+const CatchError = require('../utils/catchErrorAsyncFunc')
 
-exports.bindConsumer = async (req, res, next) => {
-    try {
-        const { nutritionistId } = req.body
-        const trainer = await Trainer.findByPk(nutritionistId)
+exports.bindConsumer = CatchError(async (req, res, next) => {
+    const { nutritionistId } = req.body
+    const trainer = await Trainer.findByPk(nutritionistId)
+    if (!trainer) next(new AppError('This trainer is not exist', 404))
 
-        if (!trainer) next(new AppError('This trainer is not exist', 404))
-        console.log(req.consumer)
-        await ConsumerTrainer.create({ consumerId: req.consumer.id, nutritionistId: trainer.id })
+    await ConsumerTrainer.create({ consumerId: req.consumer.id, nutritionistId: trainer.id })
 
-        res.status(200).json({
-            status: 'success',
-            data: '',
-        })
-    } catch (error) {
-        next(new AppError(error.message, 404))
-    }
-}
+    res.status(200).json({ status: 'success', data: '' })
+})
--- models/ProgramConsumer.js
+++ models/ProgramConsumer.js
@@ -1,8 +1,16 @@
+const { DataTypes } = require('sequelize')
 const sequelize = require('../configs/db')
+//models
 const Consumer = require('./consumerModel')
 const Program = require('./programModel')
 
-const ProgramConsumer = sequelize.define('consumer_programs', {}, { timestamps: false })
+const ProgramConsumer = sequelize.define(
+    'consumer_programs',
+    {
+        status: { type: DataTypes.INTEGER, defaultValue: 0 },
+    },
+    { timestamps: false }
+)
 
 Consumer.belongsToMany(Program, { through: ProgramConsumer })
 Program.belongsToMany(Consumer, { through: ProgramConsumer })
