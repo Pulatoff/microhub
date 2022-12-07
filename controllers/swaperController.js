133b9cd02aba2b282fc62f2b1ff935fd06e1f3a1
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-07 18:42:41 +0500

    some update

--- controllers/swaperController.js
+++ controllers/swaperController.js
@@ -5,6 +5,6 @@ const response = require('../utils/response')
 exports.addSwapIngredient = CatchError(async (req, res, next) => {
     const { food_id, ingredient_id, swap_ingredient_id } = req.body
     const { diary_id } = req.params
-    await Swaper.create({ food_id, ingredient_id, swap_ingredient_id, diaryId: diary_id })
+    await Swaper.create({ food_id, ingredient_id, swap_ingredient_id })
     response(201, 'You are successfully swap ingredient', true, '', res)
 })
