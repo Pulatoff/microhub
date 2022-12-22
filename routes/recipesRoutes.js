979dd040664076d09effe2685c466c7d17b7f030
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-22 20:59:43 +0500

    some update

--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -4,10 +4,15 @@ const auth = require('../controllers/authController')
 const controller = require('../controllers/recipeController')
 const consumer = require('../controllers/consumerController')
 
+router.route('/').get(auth.protect, controller.getAllRecipes).post(auth.protect, controller.addRecipe)
 router.route('/search').get(auth.protect, controller.searchRecipes)
 router.route('/ingredients/subsitutes/:id').get(controller.subsituteIngredients)
 router.route('/ingredients/info/:id').get(controller.getIngredientInfo)
 router.route('/ingredients/info').get(auth.protect, controller.searchIngredients)
-router.route('/:id').get(auth.protect, controller.getOneRecipe)
+router
+    .route('/:id')
+    .get(auth.protect, controller.getOneRecipe)
+    .patch(auth.protect, controller.updateRecipes)
+    .delete(auth.protect, controller.deleteRecipe)
 
 module.exports = router
