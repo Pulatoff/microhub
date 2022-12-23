3831c468d9ce6ab8a9c489448bae857164891e02
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-23 12:15:37 +0500

    some update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,21 @@
-const User = require('../models/consumerModel')
+const User = require('../models/userModel')
 // types
-const { reqUserType } = require('../types/userType')
+const { reqUserType, resUserType } = require('../types/userType')
 // utils
 const response = require('../utils/response')
 const CatchError = require('../utils/catchErrorAsyncFunc')
 
 exports.addUser = CatchError(async (req, res, next) => {
     const userData = reqUserType(req.body)
-    response(201, 'You are successfully created user', true, { userData }, res)
+    const user = await User.create(userData)
+    const newUser = resUserType(user)
+
+    response(201, 'You are successfully created user', true, { user: newUser }, res)
+})
+
+exports.getUser = CatchError(async (req, res, next) => {
+    const { id } = req.params
+    const user = await User.findByPk(id)
+    const newUser = resUserType(user)
+    response(200, 'You are successfully get one user', true, { user: newUser }, res)
 })
