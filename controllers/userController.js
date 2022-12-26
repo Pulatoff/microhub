49e37f808a233c7bf937eaa474660712cad9f2d8
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-26 11:15:45 +0500

    some updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,23 +9,42 @@ exports.addUser = CatchError(async (req, res, next) => {
 
 exports.getUser = CatchError(async (req, res, next) => {
     const { id } = req.params
-    const { sort } = req.query
+    const { sort, page, limit } = req.query
     const user = await User.findByPk(id)
     // sort models ?sort=createdAt
     // selecting fields
     // pagination fields
 
     User.findAll({
-        order: [],
-        attributes: { exclude: [] },
+        order: [], // for sorting
+        attributes: { exclude: [] }, // fort selecting fields
+        offset: 2,
+        limit: 2,
     })
     response(200, 'You are successfully get one user', true, { user }, res)
 })
 
 exports.getUsers = CatchError(async (req, res, next) => {
+    const { field } = req.query
+    const fields = field.split(',')
+
+    const attribute = {
+        exclude: [],
+        include: [],
+    }
+    fields.map((val) => {
+        if (val.startsWith('-')) {
+            attribute.exclude.push(val.slice(1))
+        } else {
+            attribute.include.push(val)
+        }
+    })
+
     const users = await User.findAll({
         order: [['id', 'DESC']],
-        attributes: { exclude: ['isActive'] },
+        attributes: attribute,
+        offset: 2,
+        limit: 2,
     })
     response(200, 'You are successfully get all users', true, { users }, res)
 })
