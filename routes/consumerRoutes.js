93beac70460edca7bfcb8e428783b7b35226b707
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-28 15:56:29 +0500

    some updates

--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -8,38 +8,55 @@ const bindProgram = require('../controllers/programConsumerController')
 
 router
     .route('/')
-    .post(authController.protect, authController.role(['consumer']), controller.addConsumer)
-    .get(authController.protect, authController.role(['nutritionist']), controller.getConsumers)
-    .patch(authController.protect, authController.role(['consumer']), controller.updateConsumer)
+    .post(authController.protect, authController.role(['consumer', 'admin']), controller.addConsumer)
+    .get(authController.protect, authController.role(['nutritionist', 'admin']), controller.getConsumers)
+    .patch(authController.protect, authController.role(['consumer', 'admin']), controller.updateConsumer)
 
 router
     .route('/trainer/accept')
     .post(
         authController.protect,
-        authController.role(['consumer']),
+        authController.role(['consumer', 'admin']),
         controller.protectConsumer,
         controller.acceptNutritioinst
     )
 
 router
     .route('/questionnaire')
-    .post(authController.protect, authController.role(['consumer']), questionaire.sendQuestionnaire)
+    .post(authController.protect, authController.role(['consumer', 'admin']), questionaire.sendQuestionnaire)
 
 router
     .route('/questionnaire/:id')
-    .patch(authController.protect, authController.role(['consumer']), questionaire.updateQuestionaire)
+    .patch(authController.protect, authController.role(['consumer', 'admin']), questionaire.updateQuestionaire)
 
-router.route('/programs').get(authController.protect, authController.role(['consumer']), bindProgram.getPrograms)
+router
+    .route('/programs')
+    .get(authController.protect, authController.role(['consumer', 'admin']), bindProgram.getPrograms)
 
-router.route('/search').get(authController.protect, bindConsumer.searchConsumer)
+router
+    .route('/search')
+    .get(authController.protect, authController.role(['admin', 'consumer']), bindConsumer.searchConsumer)
 
 router
     .route('/trainer')
-    .post(authController.protect, controller.protectConsumer, bindConsumer.bindConsumer)
-    .get(authController.protect, controller.getTrainers)
+    .post(
+        authController.protect,
+        authController.role(['consumer']),
+        controller.protectConsumer,
+        bindConsumer.bindConsumer
+    )
+    .get(authController.protect, authController.role(['consumer']), controller.getTrainers)
 
-router.route('/trainers/request').get(authController.protect, controller.getRequestedTrainers)
+router
+    .route('/trainers/request')
+    .get(authController.protect, authController.role(['consumer']), controller.getRequestedTrainers)
 
-router.route('/:id').get(authController.protect, controller.getOneConsumer)
+router
+    .route('/:id')
+    .get(
+        authController.protect,
+        authController.role(['personal_trainer', 'admin', 'nutritionist']),
+        controller.getOneConsumer
+    )
 
 module.exports = router
