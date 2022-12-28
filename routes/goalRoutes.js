5f727e12d1ed9a3642fd81af52c687f76a3e900f
Pulatoff <niyozbekpulatov@gmail.com>
2022-12-28 16:16:25 +0500

    some updates

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -6,6 +6,7 @@ router
     .route('/')
     .post(authController.protect, authController.role(['consumer']), controller.addGoal)
     .get(authController.protect, authController.role(['consumer']), controller.getAllGoals)
+
 router
     .route('/:id')
     .get(authController.protect, authController.role(['consumer']), controller.getOneGoal)
