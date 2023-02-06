5499d894a4f852134a67c14fb568fa7c9a3bd84f
Pulatoff <niyozbekpulatov@gmail.com>
2023-02-06 19:23:52 +0500

    consumer details writed

--- models/consumerDetailsModel.js
+++ models/consumerDetailsModel.js
@@ -5,27 +5,11 @@ const Consumer = require('./consumerModel')
 const ConsumerDetails = sequelize.define(
     'consumer_details',
     {
-        weight: { type: DataTypes.FLOAT },
-        height: { type: DataTypes.FLOAT },
-        least_favorite_foods: {
-            type: DataTypes.TEXT,
-            get() {
-                return this.getDataValue('least_favorite_foods').split(';')
-            },
-            set(val) {
-                this.setDataValue('least_favorite_foods', val.join(';'))
-            },
-        },
-        favorite_foods: {
-            type: DataTypes.TEXT,
-            get() {
-                return this.getDataValue('favorite_foods').split(';')
-            },
-            set(val) {
-                this.setDataValue('favorite_foods', val.join(';'))
-            },
-        },
-        tdee: { type: DataTypes.FLOAT, allowNull: false },
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        weight: { type: DataTypes.FLOAT, allowNull: false },
+        height: { type: DataTypes.FLOAT, allowNull: false },
+        from_date: { type: DataTypes.DATE, allowNull: false },
+        to_date: { type: DataTypes.DATE, allowNull: false },
     },
     { timestamps: true, createdAt: true, updatedAt: false }
 )
