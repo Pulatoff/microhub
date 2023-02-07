7e82a9c4c3cf9cafb734715ccfb19d7c30cb8c15
Pulatoff <niyozbekpulatov@gmail.com>
2023-02-07 18:09:05 +0500

    some updates

--- controllers/programController.js
+++ controllers/programController.js
@@ -239,6 +239,18 @@ exports.getMeals = CatchError(async (req, res, next) => {
             },
         ],
     })
-
-    response(200, 'You are get all meals', true, { programs: consumer?.programs }, res)
+    const meal = []
+    const meals = []
+    if (!consumer) {
+        next('You are not assign consumer to program', 400)
+    }
+    consumer?.programs.map((val) => {
+        meal.push(val.meals)
+    })
+    meal.map((val) => {
+        for (let i = 0; i < val.length; i++) {
+            meals.push(val[i])
+        }
+    })
+    response(200, 'You are get all meals', true, { meals }, res)
 })
