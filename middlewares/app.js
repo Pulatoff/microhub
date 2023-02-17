8daf4dd917ffbc2971e7a59c6ab4ba12a468eab1
Pulatoff <niyozbekpulatov@gmail.com>
2023-02-17 16:37:14 +0500

    additional cors for socket

--- middlewares/app.js
+++ middlewares/app.js
@@ -29,6 +29,9 @@ app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080/')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()
 })
 
